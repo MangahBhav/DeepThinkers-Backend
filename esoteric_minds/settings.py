352a03de091850a67eb3924c4b00a2ff38f5c6fe
KeneNwogu <nwogujoseph1111@gmail.com>
2024-04-25 15:33:09 +0100

    fix: manually add allowed_hosts

--- esoteric_minds/settings.py
+++ esoteric_minds/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('HOSTED_DOMAIN'), 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'esoteric-minds.onrender.com', 'seal-app-yf2u7.ondigitalocean.app', 'localhost']
 
 # Application definition
 
