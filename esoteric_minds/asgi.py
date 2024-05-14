4e02287130b1da0e1af8d25ed63932ee28326b17
KeneNwogu <nwogujoseph1111@gmail.com>
2024-05-14 02:36:14 +0100

    fix: allowed host validator

--- esoteric_minds/asgi.py
+++ esoteric_minds/asgi.py
@@ -11,7 +11,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
+from channels.security.websocket import AllowedHostsOriginValidator
 import posts.routing
 from .middleware import TokenAuthMiddleware
 
@@ -21,7 +21,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esoteric_minds.settings')
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
-  'websocket': TokenAuthMiddleware(URLRouter(
+  'websocket': AllowedHostsOriginValidator(TokenAuthMiddleware(URLRouter(
       posts.routing.websocket_urlpatterns
-  )),
+  ))),
 })
